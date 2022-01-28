cba9839d910dc44f33f898d29353966252837190
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-01-28 10:05:18 +0700

    change register_url_event to base module instead of geoserver monitoring

--- geonode/maps/views.py
+++ geonode/maps/views.py
@@ -71,7 +71,7 @@ from geonode.documents.models import get_related_documents
 from geonode.people.forms import ProfileForm
 from geonode.base.views import batch_modify
 from .tasks import delete_map
-from geonode.monitoring import register_event
+from geonode.base import register_event
 from geonode.monitoring.models import EventType
 from geonode.thumbs.thumbnails import create_thumbnail
 from deprecated import deprecated
