d1d49b868b9df2f199134ebe50e6956f47b281a1
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-03-08 21:01:52 +0700

    change initial migrations

--- geonode/faqs/models.py
+++ geonode/faqs/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext_noop as _
 from colorfield.fields import ColorField
 
-# Create your models here.
-
 class Site(models.Model):
   
   BOOLEAN_CHOICES = [
@@ -12,7 +10,7 @@ class Site(models.Model):
   ]
 
   header_title = models.CharField(max_length=255, help_text=_("Title Page"), default="Frequently Asked Questions")
-  header_title_color = ColorField(default="#000000")
+  header_title_color = ColorField(default="#000000", null=True)
   contents = models.TextField(null=True, blank=True, help_text=_("Content of the FAQs page"))
   authenticated_users = models.BooleanField(default=True, verbose_name="Display for authenticated users", choices=BOOLEAN_CHOICES)
   
--- geonode/faqs/views.py
+++ geonode/faqs/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from .models import Site
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
 
 def FAQView(request):
-    authenticated_faqs = Site.objects.filter(authenticated_users=True).get()
-    anonymous_faqs = Site.objects.filter(authenticated_users=False).get()
+    if Site.objects.filter(authenticated_users=True).exists():
+      authenticated_faqs = Site.objects.filter(authenticated_users=True).get()
+    else:
+      authenticated_faqs = None
     
+    if Site.objects.filter(authenticated_users=False).exists():
+      anonymous_faqs = Site.objects.filter(authenticated_users=False).get()
+    else:
+      anonymous_faqs = None
+      
     return render(request, 'faqs.html', { 'auth_faqs': authenticated_faqs, 'anon_faqs': anonymous_faqs })
