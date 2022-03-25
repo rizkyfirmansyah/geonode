c6c5e9b454e53f80ea414d613ea8b450881935d9
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-03-25 11:32:04 +0700

    toggle warning for tinymce

--- geonode/settings.py
+++ geonode/settings.py
@@ -1645,7 +1645,7 @@ TINYMCE_DEFAULT_CONFIG = {
     ],
     "template_cdate_format": '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
     "template_mdate_format": '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-    "setup": 'function(editor) {editor.on("input", onInputChange)}'
+    "setup": 'function(editor) { editor.on("paste change input undo redo", onInputChange) }'
 }
 
 # Make Free-Text Kaywords writable from users or read-only
