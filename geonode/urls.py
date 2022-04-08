7a4b97ac1a0e50887f89cd4c516a326bcbffa916
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-04-08 10:36:59 +0700

    remove developer page from urls

--- geonode/urls.py
+++ geonode/urls.py
@@ -70,10 +70,6 @@ urlpatterns = [
     url(r'^help/$',
         TemplateView.as_view(template_name='help.html'),
         name='help'),
-    url(r'^developer/$',
-        TemplateView.as_view(
-            template_name='developer.html'),
-        name='developer'),
     url(r'^about/$',
         TemplateView.as_view(template_name='about.html'),
         name='about'),
