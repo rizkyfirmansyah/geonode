5293f0d679e26e9c0adcfeadc1e654fe1adfa76f
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-04-08 13:35:19 +0700

    set default upload max size to 2 GB

--- geonode/settings.py
+++ geonode/settings.py
@@ -2122,7 +2122,7 @@ FILE_UPLOAD_HANDLERS = [
     'django.core.files.uploadhandler.TemporaryFileUploadHandler',
 ]
 
-DEFAULT_MAX_UPLOAD_SIZE = int(os.getenv('DEFAULT_MAX_UPLOAD_SIZE', 1848576000))  # 1800 MB
+DEFAULT_MAX_UPLOAD_SIZE = int(os.getenv('DEFAULT_MAX_UPLOAD_SIZE', 2048576000))  # 2 GB
 DEFAULT_BUFFER_CHUNK_SIZE = int(os.getenv('DEFAULT_BUFFER_CHUNK_SIZE', 64 * 1024))
 
 '''
