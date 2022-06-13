4f5efc4dcb851a08b89e807eb65c712842c43446
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-06-13 11:21:30 +0700

    fix group.groupprofile relations does not exists

--- geonode/groups/forms.py
+++ geonode/groups/forms.py
@@ -21,13 +21,13 @@
 from django import forms
 from slugify import slugify
 from django.utils.translation import ugettext as _
-from geonode.security.forms import ProfileMultipleChoiceField
 from modeltranslation.forms import TranslationModelForm
 from django.db.models import Q
 
 from django.contrib.auth import get_user_model
 
 from geonode.groups.models import GroupProfile
+from django.contrib.auth.models import Group
 
 
 class GroupForm(TranslationModelForm):
@@ -83,6 +83,26 @@ class GroupUpdateForm(forms.ModelForm):
         exclude = ['group']
 
 
+class ProfileMultipleChoiceField(forms.ModelMultipleChoiceField):
+  
+    def label_from_instance(self, obj):
+        full_name = ''
+        if obj.first_name and obj.last_name and obj.organization:
+            full_name = " ".join([obj.first_name, obj.last_name]) + " (" + obj.organization + ")"
+            return full_name
+        elif obj.first_name and obj.organization:
+            full_name = obj.first_name + " (" + obj.organization + ")"
+            return full_name
+        elif obj.first_name and obj.last_name:
+            full_name = " ".join([obj.first_name, obj.last_name])
+            return full_name
+        elif obj.first_name:
+              full_name = obj.first_name
+              return full_name
+        else:
+            return obj.username
+
+
 class GroupMemberForm(forms.Form):
     get_users = get_user_model().objects.all().exclude(Q(username='AnonymousUser'))
 
@@ -122,3 +142,66 @@ class GroupMemberForm(forms.Form):
                 }
             )
         return new_members
+
+
+class GroupsMultipleChoiceField(forms.MultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.title
+
+
+def get_groups_choices():
+    get_groups_choices = [(i.slug, i.name) for i in GroupProfile.objects.all()]
+    
+    return get_groups_choices
+
+
+class PermissionsForm(forms.Form):
+    get_users = get_user_model().objects.all().exclude(Q(username='AnonymousUser'))
+
+    def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      for field in self.fields:
+          self.fields[field].widget.attrs.update(
+              {
+                  'class': 'selectpicker',
+                  'data-live-search': 'true',
+                  'data-selected-text-format': 'count > 4',
+                  'data-actions-box': 'true',
+                  'data-size': '5'})
+
+    view_resourcebase_users = ProfileMultipleChoiceField(
+      label="The following users",
+      queryset=get_users,
+      to_field_name="username",
+      required=False)
+    view_resourcebase_groups = GroupsMultipleChoiceField(
+      label="The following groups",
+      choices=get_groups_choices,
+      required=False)
+    download_resourcebase_users = ProfileMultipleChoiceField(
+      label="The following users",
+      queryset=get_users,
+      to_field_name="username",
+      required=False)
+    download_resourcebase_groups = GroupsMultipleChoiceField(
+      label="The following groups",
+      choices=get_groups_choices,
+      required=False)
+    change_resourcebase_metadata_users = ProfileMultipleChoiceField(
+      label="The following users",
+      queryset=get_users,
+      to_field_name="username",
+      required=False)
+    change_resourcebase_metadata_groups = GroupsMultipleChoiceField(
+      label="The following groups",
+      choices=get_groups_choices,
+      required=False)
+    manage_resourcebase_users = ProfileMultipleChoiceField(
+      label="The following users",
+      queryset=get_users,
+      to_field_name="username",
+      required=False)
+    manage_resourcebase_groups = GroupsMultipleChoiceField(
+      label="The following groups",
+      choices=get_groups_choices,
+      required=False)
--- geonode/themes/context_processors.py
+++ geonode/themes/context_processors.py
@@ -3,7 +3,7 @@ from django.core.cache import cache
 
 from geonode.datasets.forms import RodaForm
 from geonode.people.forms import ProfileSignupForm, ProfileLoginForm, ProfileChangePasswordForm, ProfileAddEmailForm
-from geonode.security.forms import PermissionsForm
+from geonode.groups.forms import PermissionsForm
 from .forms import FeedbackForm
 from .models import GeoNodeThemeCustomization, THEME_CACHE_KEY
 
