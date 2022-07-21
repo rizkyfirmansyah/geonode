9586177a9e463caeadec44b6c68010ebdbf789d0
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2022-07-21 20:05:44 +0700

    add libraries staticfiles to prevent error 'TemplateSyntaxError: 'staticfiles' is not a registered tag library'

--- geonode/settings.py
+++ geonode/settings.py
@@ -719,6 +719,9 @@ TEMPLATES = [
             #      'django.template.loaders.filesystem.Loader',
             #      'django.template.loaders.app_directories.Loader',
             # ],
+            'libraries' : {
+                'staticfiles': 'django.templatetags.static', 
+            },
             'debug': DEBUG,
         },
     },
