3543f14b4251896a7258258090096ee026c78529
Rizky Firmansyah <rizkyfirmansyah@hotmail.com>
2023-04-10 23:11:25 +0700

    remove .annotate(Sum('pk')) to prevent error when grouping the messages

--- geonode/static/geonode/js/upload/LayerInfo.js
+++ geonode/static/geonode/js/upload/LayerInfo.js
@@ -544,28 +544,6 @@ define(function(require, exports) {
                     }
                 }
             });
-        } else if (resp.status === "incomplete") {
-            var id = common.parseQueryString(resp.url).id;
-            var element = 'next_step_' + id
-            var a = '<a id="' + element + '" class="btn btn-primary" target="_blank">Continue</a>';
-            var msg = '<p>' + gettext('Files are ready to be ingested!')
-
-            if (resp.redirect_to.indexOf('time') !== -1 || resp.url.indexOf('time') !== -1) {
-                msg += '&nbsp;' + gettext('A temporal dimension may be added to this Layer.') + '&nbsp;' + a + '</p>'
-            } else {
-                msg += '</p>'
-            }
-
-            self.logStatus({
-                msg: msg,
-                level: 'alert-info',
-                empty: 'true'
-            });
-            $("#" + element).on('click', resp, self.doResume);
-
-            callback(array);
-
-            return;
         } else if (resp.status === "other") {
             self.logStatus({
                 msg: '<p class="text-center">' + gettext('You need to specify more information in order to complete your upload') + '</span>',
